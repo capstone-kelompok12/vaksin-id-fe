d6621d883066eebc698c71431b7a48334d71939e
anangkf_ <gonanggoneng@gmail.com>
2022-12-06 14:21:48 +0700

    fixing ui page vaccine stock

--- src/components/ModalAddEditStock.jsx
+++ src/components/ModalAddEditStock.jsx
@@ -1,94 +1,121 @@
 import React, { useState } from 'react'
-import { Box, Button, IconButton, Modal, TextField, Typography } from '@mui/material'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, IconButton, InputLabel, MenuItem, Select, Stack, TextField} from '@mui/material'
 import AddIcon from "@mui/icons-material/Add";
-import EditIcon from "@mui/icons-material/Edit";
+import ModeEditOutlinedIcon from '@mui/icons-material/ModeEditOutlined';
+import { useEffect } from 'react';
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  width: 767,
-  p: 4,
-};
+const INITIAL_FORM_DATA = {
+  id: '',
+  nama: '',
+  dosis: '',
+  stok: 0
+}
 
-const ModalAddEditStock = ({edit}) => {
+const ModalAddEditStock = ({data, edit}) => {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState("");
-  // const [stock, setStock] = useState("");
+  const [formData, setFormData] = useState(INITIAL_FORM_DATA);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const handleChange = (e) =>{
+    const {name, value} = e.target
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  const {nama, dosis, stok} = formData
+
+  useEffect(() =>{
+    if(data){
+      setFormData(data)
+    }
+    return () =>{
+      setFormData(INITIAL_FORM_DATA)
+    }
+  }, [data])
+  
   return (
     <>
       {edit
-      ? <IconButton
+      ? <IconButton 
+        color='info'
         onClick={handleOpen}
         aria-label="edit"
-        sx={{backgroundColor: 'success.light', borderRadius: 2, '&:hover': {backgroundColor: 'success.main'}}}
+        sx={{border: '1px solid'}} 
       >
-        <EditIcon />
+        <ModeEditOutlinedIcon />
       </IconButton>
       : <Button
-        sx={{
-          borderRadius: 2,
-        }}
         variant="outlined"
         onClick={handleOpen}
         startIcon={<AddIcon />}
       >
         Tambah Vaksin
       </Button>}
-      <Modal
+
+      <Dialog
+        fullWidth
+        maxWidth='xs'
         open={open}
         onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
       >
-        <Box
-          style={{
-            backgroundColor: "#EEF6ED"
-          }}
-          sx={style}
-        >
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            {edit ? 'Edit Vaksin' : 'Tambah Vaksin'}
-          </Typography>
-          <TextField
-            sx={{
-              marginTop: 5,
-              width: 360,
-            }}
-            id="outlined-basic"
-            label="Nama Vaksin"
-            variant="outlined"
-            placeholder="Masukkan Nama"
-          />
-          <TextField
-            sx={{
-              marginTop: 4,
-              width: 360,
-            }}
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            helperText={!value ? "" : "Masukkan Stok Vaksin"}
-            label="Stok Vaksin"
-            variant="outlined"
-            placeholder="Masukkan Stok"
-          />
-          <Box
-            sx={{
-              marginTop: 10,
-              marginLeft: 65,
-            }}
-          >
-            <Button variant="outlined">Batal</Button>
-            <Button variant="contained" sx={{ml: 2}}>Tambah</Button>
-          </Box>
-        </Box>
-      </Modal>
+        <DialogTitle sx={{alignSelf: 'center'}}>{edit ? 'Edit' : 'Tambah'} Stok Vaksin</DialogTitle>
+        <DialogContent>
+          <Stack spacing={3} sx={{py: 2}}>
+            <FormControl fullWidth>
+              <InputLabel id='label-vaksin'>Vaksin</InputLabel>
+              <Select
+                label='Vaksin'
+                // labelId='label-vaksin'
+                id='select-vaksin'
+                name='nama'
+                value={nama}
+                onChange={handleChange}
+              >
+                <MenuItem value='AstraZeneca'>AstraZeneca</MenuItem>
+                <MenuItem value='Novavax'>Novavax</MenuItem>
+                <MenuItem value='Sinovac'>Sinovac</MenuItem>
+                <MenuItem value='Moderna'>Moderna</MenuItem>
+                <MenuItem value='Janssen'>Janssen</MenuItem>
+                <MenuItem value='Pfizer'>Pfizer</MenuItem>
+                <MenuItem value='Sputnik'>Sputnik</MenuItem>
+                <MenuItem value='Sinopharm'>Sinopharm</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel id='label-dosis'>Dosis</InputLabel>
+              <Select
+                label='Vaksin'
+                // labelId='label-dosis'
+                id='select-dosis'
+                name='dosis'
+                value={dosis}
+                onChange={handleChange}
+              >
+                <MenuItem value='Pertama'>Pertama</MenuItem>
+                <MenuItem value='Kedua'>Kedua</MenuItem>
+                <MenuItem value='Ketiga'>Ketiga</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField 
+              // labelId='label-stok'
+              id='input-stok'
+              label='Stok Vaksin'
+              type={'number'}
+              name='stok'
+              value={stok}
+              onChange={handleChange}
+            />
+          </Stack>
+        </DialogContent>
+        <DialogActions sx={{p: 3}}>
+          <Button variant='outlined'>Batal</Button>
+          <Button variant='contained'>{edit ? 'Simpan' : 'Tambah'}</Button>
+        </DialogActions>
+      </Dialog>
     </>
   )
 }
