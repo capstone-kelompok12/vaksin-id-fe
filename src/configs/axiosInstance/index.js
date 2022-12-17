c71d0a427981ef28fc8dea26465772f1b76ad2eb
anangkf_ <gonanggoneng@gmail.com>
2022-12-17 22:35:20 +0700

    fixed error axios interceptors

--- src/configs/axiosInstance/index.js
+++ src/configs/axiosInstance/index.js
@@ -2,19 +2,15 @@ import axios from "axios";
 import { CONST } from "../../utils/constants";
 import Auth from '../../utils/Auth'
 
-const token = Auth.getToken()
 const config = {
   baseURL: CONST.BASE_URL_API,
-  // headers:{
-  //   'api-key-name': CONST.API_KEY
-  // }
 }
 
 const axiosInstance = axios.create(config)
 axiosInstance.interceptors.request.use(
   (req) => {
     if(req.url.includes('vaccine') || req.url.includes('sessions')){
-      req.headers['Authorization'] = `Bearer ${token}`
+      req.headers.Authorization = `Bearer ${Auth.getToken()}`
     }
     return req;
   },
